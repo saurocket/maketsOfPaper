637c24a094c412279b58ef50c846e5c4c3b19c70
Yauheni Luzakou <luzakou@yandex.ru>
2021-07-13 23:36:15 +0200

    fix image

--- src/store/galleryObjects/galleryImages.ts
+++ src/store/galleryObjects/galleryImages.ts
@@ -1,18 +1,27 @@
+import photo1 from '../../img/gallery/arch/photo1.jpg'
+import photo2 from '../../img/gallery/arch/photo2.jpg'
+import photo3 from '../../img/gallery/imed/photo3.jpg'
+import photo4 from '../../img/gallery/transport/photo4.jpg'
+import photo5 from '../../img/gallery/transport/photo5.jpg'
+import photo6 from '../../img/gallery/presentation/photo6.jpg'
+import photo7 from '../../img/gallery/presentation/photo7.jpg'
+import photo8 from '../../img/gallery/imed/photo8.jpg'
+
 export const transportImage = [
-    {id: '1', imageName: '/gallery/transport/photo4.jpg', tag: 'transport'},
-    {id: '2', imageName: '/gallery/transport/photo4.jpg', tag: 'transport'}
+    {id: '1', imageName: photo4, tag: 'transport'},
+    {id: '2', imageName: photo5, tag: 'transport'}
 ]
 export const archImage = [
-    {id: '3', imageName: '/gallery/arch/photo1.jpg', tag: 'arch'},
-    {id: '4', imageName: '/gallery/arch/photo2.jpg', tag: 'arch'}
+    {id: '3', imageName: photo1, tag: 'arch'},
+    {id: '4', imageName: photo2, tag: 'arch'}
 ]
 export const imedImage = [
-    {id: '5', imageName: '/gallery/imed/photo3.jpg', tag: 'imed'},
-    {id: '6', imageName: '/gallery/imed/photo8.jpg', tag: 'imed'}
+    {id: '5', imageName: photo3, tag: 'imed'},
+    {id: '6', imageName: photo8, tag: 'imed'}
 ]
 export const presentationImage = [
-    {id: '7', imageName: '/gallery/presentation/photo6.jpg', tag: 'presentation'},
-    {id: '8', imageName: '/gallery/presentation/photo7.jpg', tag: 'presentation'}
+    {id: '7', imageName: photo6, tag: 'presentation'},
+    {id: '8', imageName: photo7, tag: 'presentation'}
 ]
 
 export type ImageType = {id: string, imageName: string, tag: string}
